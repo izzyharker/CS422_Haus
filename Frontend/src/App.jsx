f928e6f849f0eb0efcb38013e6370fedb0d34ff9
Izzy Harker <harkerisabella@gmail.com>
2024-03-06 16:30:01 -0800

    added login to frontend - entering any values will do the same thing rn so dw about it

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,16 +3,49 @@ import "./components/NavBar/NavBar"
 import NavBar from './components/NavBar/NavBar'
 import ChoreContainer from "./components/ChoreCards/ChoreContainer"
 import HouseContainer from "./components/House/HouseContainer"
+import { useState } from 'react'
 
 function App() {
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [userName, setUserName] = useState({})
+
+	// API call: this needs to login the correct user
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        setIsSubmitted(true)
+		setUserName("default")
+    }
+
+    const loginForm = (
+        <div className="login">
+			<div className="form">
+				<h1>Enter HAUS</h1>
+				<form onSubmit={handleSubmit}>
+					<div className="input-container">
+						<label>Username </label>
+						<input type="text" name="uname" required />
+					</div>
+					<div className="input-container">
+						<label>Password </label>
+						<input type="password" name="pass" required />
+					</div>
+					<div className="button-container">
+						<input type="submit" value="Login"/>
+					</div>
+				</form>
+			</div>
+        </div>
+     );
+
 	return (
 		<div className="haus">
 			< NavBar />
 
-			<div className="haus-content">
-				<ChoreContainer />
-				<HouseContainer />
-			</div>
+			{isSubmitted ? <div className="haus-content">
+					<ChoreContainer user={userName}/>
+					<HouseContainer user={userName}/>
+				</div> : loginForm}
 		</div>
 	)
 }
