39153b2c9bb33f87aeb6e7ccc3b9823980c8081d
unknown <andrew.the10th@gmail.com>
2024-03-07 20:59:58 -0800

    Figured out sending a json :)

--- flask_integration.py
+++ flask_integration.py
@@ -8,5 +8,12 @@ app = Flask(__name__, static_folder="Frontend/src")
 def get_data():
     return jsonify({'data': 'Your data here'})
 
+
+# {fetch('http://localhost:5000/api/data')
+#             .then(response => response.json())
+#             .then(data => console.log(data))
+#             }
+
+
 if __name__ == '__main__':
     app.run(debug=True)
